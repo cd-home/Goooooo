befa17b613a17295d3989a74bfc5316cb9d6ac66
GodYao <liyaoo1995@gmail.com>
2022-04-08 17:46:05 +0800

    😀🌴⭐🌎☀️🌈🌽: prod mode

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,9 +3,11 @@ package config
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/GodYao1995/Goooooo/pkg/config"
 	"github.com/GodYao1995/Goooooo/pkg/errno"
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 	"go.uber.org/fx"
 )
@@ -17,17 +19,19 @@ func NewConfig() *viper.Viper {
 	app, mode, configPaths := command()
 
 	// Production Mode Use the environment variables
-	if _app, _mode := _differentAppAndEnvironment(); _app != "" && _mode != "" {
-		app = _app
-		mode = _mode
-	}
+	_differentAppAndEnvironment(&app, &mode)
 
-	if app == "" && mode == "" {
-		log.Fatalln(errno.NotFoundService)
-	}
 	return config.NewViper(app, mode, configPaths.GetNames()...)
 }
 
-func _differentAppAndEnvironment() (string, string) {
-	return os.Getenv("APP_NAME"), os.Getenv("APP_MODE")
+func _differentAppAndEnvironment(app, mode *string) {
+	_app, _mode := os.Getenv("APP_NAME"), os.Getenv("APP_MODE")
+	if len(strings.TrimSpace(_app)) != 0 && len(strings.TrimSpace(_mode)) != 0 {
+		*app = _app
+		*mode = _mode
+	}
+
+	if len(strings.TrimSpace(*app)) == 0 || len(strings.TrimSpace(*mode)) == 0 {
+		log.Fatalln(errno.NotFoundService)
+	}
 }
